0ab3708a202c6b1d48d3238a960a2bc1cf542b3a
You-Sheng Yang <vicamo@gmail.com>
2017-03-08 10:32:33 +0800

    really prepare cross build environment

--- update.sh
+++ update.sh
@@ -4,11 +4,22 @@
 
 SUITE=stretch
 
+QEMU_ARCH_amd64=x86_64
+QEMU_ARCH_arm64=aarch64
+QEMU_ARCH_armel=arm
+QEMU_ARCH_armhf=arm
+QEMU_ARCH_lpia=i386
+QEMU_ARCH_powerpc=ppc
+QEMU_ARCH_powerpcspe=ppc
+QEMU_ARCH_ppc64el=ppc64le
+
 for f in $(find . -type f -name Dockerfile); do
   [ -n "$(head -n 1 $f | grep '^#.*GENERATED FROM')" ] || continue;
 
   arch=$(echo "$f" | cut -d/ -f2)
+  eval "qemu_arch=\${QEMU_ARCH_${arch}:-${arch}}"
   cat Dockerfile.template | \
     sed -e "s,@IMAGE_SUITE@,${SUITE},g" \
-      -e "s,@IMAGE_ARCH@,${arch},g" > "$f"
+      -e "s,@IMAGE_ARCH@,${arch},g" \
+      -e "s,@QEMU_ARCH@,${qemu_arch},g" > "$f"
 done
