b12c20245fcf4bf69ad1ee6bb9c33b1f8876296b
You-Sheng Yang <vicamo@gmail.com>
2017-10-25 10:58:54 +0800

    obsolete Dockerfile.template

--- update.sh
+++ update.sh
@@ -11,12 +11,23 @@ QEMU_ARCH_powerpc=ppc
 QEMU_ARCH_powerpcspe=ppc
 QEMU_ARCH_ppc64el=ppc64le
 
-for f in $(find . -type f -name Dockerfile); do
-  [ -n "$(head -n 1 $f | grep '^#.*GENERATED FROM')" ] || continue;
+for dockerfile in $(find . -type f -name Dockerfile); do
+  [ -n "$(head -n 1 $dockerfile | grep '^#.*GENERATED FROM')" ] || continue;
 
-  arch=$(echo "$f" | cut -d/ -f2)
+  arch=$(echo "$dockerfile" | cut -d/ -f2)
   eval "qemu_arch=\${QEMU_ARCH_${arch}:-${arch}}"
-  cat Dockerfile.template | \
-    sed -e "s,@IMAGE_ARCH@,${arch},g" \
-      -e "s,@QEMU_ARCH@,${qemu_arch},g" > "$f"
+
+  cat > "$dockerfile" <<EOD
+# DO NOT EDIT!!! GENERATED FROM Dockerfile.template.
+FROM laarid/native-build:amd64
+
+RUN sudo dpkg --add-architecture ${arch} \\
+	&& sudo apt-get update -qq \\
+	&& sudo apt-get upgrade -y \\
+	&& sudo apt-get install --no-install-recommends -y --allow-unauthenticated \\
+		crossbuild-essential-${arch}
+
+ADD qemu-${qemu_arch}-static /usr/bin/qemu-${qemu_arch}-static
+EOD
+
 done
